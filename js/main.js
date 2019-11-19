b092bf8bc297abcbbd6ccbfa337e0cd4e93217c2
AsthmaticCanary <biblestudy.lokoja@gmail.com>
2019-11-19 23:51:47 +1000

    Content and Styles update
    + Added News and Store pages
    + Made pages more dynamic
    - Made broken elements more obvious :(

--- js/main.js
+++ js/main.js
@@ -1,5 +1,8 @@
-window.onload = function() {
-	
-	
+function loadDynamicObjects() {
+
+    document.title += ' | Legit News Corp.';
+    document.getElementById('copyright').innerHTML =
+    'Site design / logo &copy; 2019 Legit News Corps; In part with ZGIwMw.<br \>' +
+    '22 / 03 / 2019';
 	
 }
